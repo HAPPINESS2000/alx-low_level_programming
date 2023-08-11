ba98ad55e2edbec418d8ccf0967bf0b6b2ed6b57
HAPPINESS2000 <ijeomahappiness49@gmail.com>
2023-08-11 12:53:53 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+#include <unistd.h>
+/**
+ *  * main - Entry point
+ *   * Return: 0 (success)
+ *    */
+Int main (void)
+{
+	 fPuts("and that piece of art is useful\n"......",  stdout);
+	 return (1);
+}
