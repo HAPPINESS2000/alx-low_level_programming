208433e513ed465ac06790be4d3454fea70c588f
HAPPINESS2000 <ijeomahappiness49@gmail.com>
2023-08-13 12:58:39 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Prints the last digit of a randomly generated number
- * and whether it is greater than 5, less than 6, or 0.
+ *        and whether it is greater than 5, less than 6, or 0.
  *
  * Return: Always 0.
  */
@@ -31,6 +31,7 @@ int main(void)
 			n, n % 10);
 	}
 
-	return (0);
+	return (0);
 
 }
+
