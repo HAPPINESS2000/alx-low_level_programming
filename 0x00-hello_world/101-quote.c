52822209a75228a2444908866376bd5d4e3eb0c0
HAPPINESS2000 <ijeomahappiness49@gmail.com>
2023-08-14 14:29:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *  * main - Entry point
- *   *
- *    * Description: Prints a specific message to the standard error.
- *     * Return: Always 1 (Error)
- *      */
+ * main - A c program that prints a line to the standard error
+ * Return 1 (Success)
+ */
 int main(void)
 {
-		char *message =
-				"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-			ssize_t len = sizeof(
-							"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
-				ssize_t written = write(2, message, len);
-				if (written != len)
-						{
-									return (1);
-										}
-				return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
