3c181cd0eeb54993292f415a4698d08d7b701b92
HAPPINESS2000 <ijeomahappiness49@gmail.com>
2023-08-16 16:11:08 -0700

    iii

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
@@ -43,6 +44,5 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-}
 }
 
