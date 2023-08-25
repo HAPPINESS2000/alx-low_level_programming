c835324cab96b84947467ef6f04cd4885dc6c743
HAPPINESS2000 <ijeomahappiness49@gmail.com>
2023-08-25 14:47:08 -0700

    Shebang

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,11 +11,11 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	i = 0
-		while (dest[i] != '\0')
-		{
-			i++;
-		}
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 	j = 0;
 	while (src[j] != '\0')
 	{
@@ -23,6 +23,8 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
+
